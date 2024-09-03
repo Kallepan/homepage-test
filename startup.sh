a1784490962cd98719c2121257fa14d2670ae2d5
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2024-09-03 08:34:59 +0200

    Update file paths for icons in config and startup script

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 docker compose down
 docker compose up --force-recreate --remove-orphans -d
 docker cp ./config homepage:/app/config
-docker cp ./images homepage:/app/public/images
+docker cp ./icons homepage:/app/public/icons
 docker compose restart
